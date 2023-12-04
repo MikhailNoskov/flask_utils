913f089631f84741a696afedb33c7abc7f3cfc18
Mikhail Noskov <noskovmm@gmail.com>
2023-12-04 21:25:19 +0100

    template inheritance added

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from my_app.hello.views import hello
+from my_app.products.views import products_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(hello)
+app.register_blueprint(products_blueprint)
---
+++ my_app/products/views.py
@@ -0,0 +1,19 @@
+from werkzeug.exceptions import abort
+from flask import Blueprint, render_template
+from my_app.products.models import PRODUCTS
+
+products_blueprint = Blueprint('product', __name__)
+
+
+@products_blueprint.route('/')
+@products_blueprint.route('/home')
+def home():
+    return render_template('home.html', products=PRODUCTS)
+
+
+@products_blueprint.route('/product/<key>')
+def get_product(key):
+    product = PRODUCTS.get(key)
+    if not product:
+        abort(404)
+    return render_template('product.html', product=product)
