c39b85f03a0ce19d0dd360d8c7b73236acc16134
Mikhail Noskov <noskovmm@gmail.com>
2023-12-05 02:40:21 +0100

    redis installed and added

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from redis import Redis
+redis = Redis()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
