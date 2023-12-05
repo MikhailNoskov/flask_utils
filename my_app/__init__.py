cfe166a96c6a6f93bc31be627bde614529db09b0
Mikhail Noskov <noskovmm@gmail.com>
2023-12-05 01:29:41 +0100

    Product model added

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+
+from my_app.catalog.views import catalog
+app.register_blueprint(catalog)
+
 with app.app_context():
     db.create_all()
+
