883de9edc64552d887476553001107e0e847eef3
Mikhail Noskov <noskovmm@gmail.com>
2023-12-05 03:02:35 +0100

    models changed to mongo documents

--- my_app/catalog/models.py
+++ my_app/catalog/models.py
@@ -1,29 +1,12 @@
+import datetime
 from my_app import db
 
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    price = db.Column(db.Float)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    category = db.relationship('Category', backref=db.backref('products', lazy='dynamic'))
-    company = db.Column(db.String(100))
-
-    def __init__(self, name, price, category):
-        self.name = name
-        self.price = price
-        self.category = category
+class Product(db.Document):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    key = db.StringField(max_length=255, required=True)
+    name = db.StringField(max_length=255, required=True)
+    price = db.DecimalField()
 
     def __repr__(self):
         return f'<Product {self.id}>'
-
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return f'<Category {self.id}>'
