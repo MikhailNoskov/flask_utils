92f6dd90a50710abe9b76dd07292904ecbd6dba1
Mikhail Noskov <noskovmm@gmail.com>
2023-12-11 14:04:37 +0100

    user model added

---
+++ my_app/auth/models.py
@@ -0,0 +1,20 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, EqualTo
+
+from my_app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    pwdhash = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.pwdhash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
+
